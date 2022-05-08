d5ad5bf389d32dfb5e825605032ee3495e6e1540
mberrueta <matiasberrueta@gmail.com>
2022-05-07 21:06:32 -0300

    Moved security to lib

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,39 +3,19 @@
 class ApplicationController < ActionController::API
   before_action :authorized
 
-  def encode_token(payload)
-    JWT.encode(payload, 's3cr3t')
-  end
-
   def auth_header
     # { Authorization: 'Bearer <token>' }
     request.headers['Authorization']
   end
 
-  def decoded_token
-    return unless auth_header
-
-    token = auth_header.split[1]
-    # header: { 'Authorization': 'Bearer <token>' }
-    begin
-      JWT.decode(token, 's3cr3t', true, algorithm: 'HS256')
-    rescue JWT::DecodeError
-      nil
+  def current_user
+    @current_user ||= begin
+      payload = Security.decode(auth_header)&.first
+      User.find_by(id: payload['user_id']) if payload
     end
   end
 
-  def logged_in_user
-    return unless decoded_token
-
-    user_id = decoded_token[0]['user_id']
-    @user = User.find_by(id: user_id)
-  end
-
-  def logged_in?
-    !!logged_in_user
-  end
-
   def authorized
-    render json: { message: 'Please log in' }, status: :unauthorized unless logged_in?
+    render json: { message: 'Please log in' }, status: :unauthorized unless current_user
   end
 end
