400a34379ee8416286d271fa743b6fbb57124569
mberrueta <matiasberrueta@gmail.com>
2022-05-08 11:33:43 -0300

    ci38

--- config/initializers/load_secrets.rb
+++ config/initializers/load_secrets.rb
@@ -3,5 +3,5 @@
 begin
   SECRETS = YAML.load_file(Rails.root.join('config/secrets.yml'))[Rails.env]
 rescue StandardError
-  SECRETS = { jwt_key: 'unsecure' }.freeze
+  SECRETS = { 'jwt_key' => 'unsecure' }.freeze
 end
